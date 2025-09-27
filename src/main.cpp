01cc5a425720c4740d7bb8531302efa20333f75e
Ramón de León <ramondeleonca@gmail.com>
2025-09-26 20:16:34 -0600

    added VERY simple auto

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 // * Subsystems
 // Chassis
 const int maxVoltageMv = 12 * 1000;
+const float chassisDriveConstants[7] = {9.0, 0.0, 0.0, 250.0, 1.0, 0.0, 0.0};
+const float chassisTurnConstants[7] = {1.75, 0.0, 0.0, 300.0, 1.0, 0.0, 0.0};
 lightning::TankChassis chassis(
 	lightning::tank_odom_e_t::NO_ODOM,
 	{-1, -2, -3, -4}, // Left motors
@@ -22,7 +24,31 @@ void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {}
+void autonomous() {
+	chassis.set_drive_constants(
+		chassisDriveConstants[0],
+		chassisDriveConstants[1],
+		chassisDriveConstants[2],
+		chassisDriveConstants[3],
+		chassisDriveConstants[4],
+		chassisDriveConstants[5],
+		chassisDriveConstants[6]
+	);
+
+	chassis.set_turn_constants(
+		chassisTurnConstants[0],
+		chassisTurnConstants[1],
+		chassisTurnConstants[2],
+		chassisTurnConstants[3],
+		chassisTurnConstants[4],
+		chassisTurnConstants[5],
+		chassisTurnConstants[6]
+	);
+
+	chassis.set_brake(pros::E_MOTOR_BRAKE_BRAKE);
+
+	chassis.drive_distance(10, 0);
+}
 
 void opcontrol() {
 	// Controller
